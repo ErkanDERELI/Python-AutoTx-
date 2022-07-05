562850914dba7a7e10ac49cf0567040e93fe7031
ErkanDERELİ <75272957+ErkanDERELI@users.noreply.github.com>
2022-07-05 21:55:09 +0300

    Create tx_1.py

---
+++ tx_1.py
@@ -0,0 +1,15 @@
+import subprocess
+import time
+
+
+#file1=open("CREATE1.txt","a")
+start = time.time()
+a=0
+while a<=9999999999999999999999:
+    print('===================================================================== T E S T 1  =======================================')
+    print('=================================<<<< KYVE TESTNET TÜRKİYE  >>>>========================================================')
+    print('===============================================================' 'TX=', a, '============================================')
+    cmd = "kyved tx bank send kyve1w0rxvf6mq349cy03zwj69d2m534qyuymnvmwkl  kyve1ecdhlulwxgh2uxlnz286egyphswdzau0rtxzha 100000tkyve --chain-id korellia --fees 200tkyve --note KYVE-TÜRKİYE-TX-RİNG_1_TO_2 --keyring-backend test -y"
+    subprocess.Popen(cmd, shell=True)
+    time.sleep(3)
+    a=a+1  
