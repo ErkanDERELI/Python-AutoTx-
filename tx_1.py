963c67866b24785a8a921b757f5080905896341f
ErkanDERELİ <75272957+ErkanDERELI@users.noreply.github.com>
2022-07-05 21:56:21 +0300

    Update tx_1.py

--- tx_1.py
+++ tx_1.py
@@ -9,7 +9,7 @@ while a<=9999999999999999999999:
     print('===================================================================== T E S T 1  =======================================')
     print('=================================<<<< KYVE TESTNET TÜRKİYE  >>>>========================================================')
     print('===============================================================' 'TX=', a, '============================================')
-    cmd = "kyved tx bank send kyve1w0rxvf6mq349cy03zwj69d2m534qyuymnvmwkl  kyve1ecdhlulwxgh2uxlnz286egyphswdzau0rtxzha 100000tkyve --chain-id korellia --fees 200tkyve --note KYVE-TÜRKİYE-TX-RİNG_1_TO_2 --keyring-backend test -y"
+    cmd = "kyved tx bank send kyve1w0rxvf6mq349cy03zwj69d2m534qyuymnvmwkl  kyve1ecdhlulwxgh2uxlnz286egyphswdzau0rtxzha 100000tkyve --chain-id korellia --fees 200tkyve --note KYVE-TÜRKİYE-TX---- --keyring-backend test -y"
     subprocess.Popen(cmd, shell=True)
     time.sleep(3)
     a=a+1  
